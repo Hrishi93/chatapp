ac0c2b09715fc50ba009c44d0fec23ee240e3733
Hrishi93 <sonumahure@gmail.com>
2024-11-17 08:06:20 +0530

    first commit

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const path=require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
+
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 io.on('connection', (socket) => {
